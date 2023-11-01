8f0749522f94dd5ac9e643febd3eadb65d63c8ab
Talha Rehman <talha.9197483@talmeez.pk>
2023-11-01 02:11:24 -0700

    first commit

--- server.js
+++ server.js
@@ -1,33 +1,78 @@
-const { randomUUID } = require("crypto");
-const http = require("http");
-const { WebSocketServer } = require("ws");
+const app = require("express")();
+const httpServer = require("http").createServer(app);
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-const httpServer = http.createServer();
+app.use(cors());
 
-// 1. Setting up websocket server
-const wsServer = new WebSocketServer({ server: httpServer });
+// 1. setting up socket.io server
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+// 3. On receiving the request, if the request agrees, it connects with the client
+io.on("connection", (socket) => {
+  console.log(`socket is connected ${socket.id}`);
 
-// 3. On receiving a new connection
-wsServer.on("connection", function (ws) {
-  // received a new connection
-  console.log("received a new connection");
+// 5. on listening the custom event "join_room", server connects to the room using room data sent by client
+socket.on('join_room',(data)=>{
+  // console.log('room info received',data);
+  socket.join(data)
+})
 
-  // 6. server receives the incoming input message in form of buffer and convert it into string
-  ws.on("message", function (message) {
-    console.log(message.toString());
-  });
+  // 7. When the custom event "send_message" emitted by the client reaches to server, it listens it and get the message
+  socket.on("send_message", (data) => {
+    console.log(data);
 
-  // 7. sending message to client
-  ws.send("I am a server");
-
-  //   10. When client closed the connection, it shows the message 'disconnected'
-  ws.on("close", function () {
-    console.log("disconnected");
+    // 8. server is sending a message to all the clients that join the room except that room member that sends the message by emitting an custom event named "reply" with the message it wants to send on the specific room .
+    socket.to(data.room).emit("reply", data.message);
   });
 });
 
-const port = 8000;
-
-httpServer.listen(port, () => {
-  console.log(`Websocket server is listening on port ${port}`);
+httpServer.listen(8000, () => {
+  console.log("server is running on port 8000");
 });
+
+
+
+
+/*---------------------------------------
+-----------------------------------------
+A simple socket.io server example without room
+----------------------------------------
+----------------------------------------
+*/
+
+// const app = require("express")();
+// const httpServer = require("http").createServer(app);
+// const { Server } = require("socket.io");
+// const cors = require("cors");
+
+// app.use(cors());
+
+// 1. setting up socket.io server
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST"],
+//   },
+// });
+// 3. On receiving the request, if the request agrees, it connects with the client
+// io.on("connection", (socket) => {
+//   console.log(`socket is connected ${socket.id}`);
+
+// 5. When the custom event "send_message" emitted by the client reaches to server, it listens it and get the message
+// socket.on("send_message",(data)=>{
+//   console.log(data)
+
+//   6. server is sending a message to client by emitting an custom event named "reply" with the message it wants to send.
+//   socket.emit("reply",'I received your message')
+// })
+
+// });
+
+// httpServer.listen(8000, () => {
+//   console.log("server is running on port 8000");
+// });
